627f1dc9c9c6072b17918c6386e1147f4f566467
Marrwan <Abdulbasitdamilola6@gmail.com>
2021-05-11 09:41:04 +0100

    added async functions

--- controllers/index.js
+++ controllers/index.js
@@ -1,40 +1,33 @@
 const axios = require("axios");
 // LOAD MODELS
 
-
 const Subject = require("../models/Subject");
 const User = require("../models/User");
 const AppError = require("../utilities/appError");
 
 exports.getHomepage = async (req, res) => {
   try {
-    // await MSSNNews.find({}, async (err, mssnnews) => {
-    //   await AcademicNews.find({}, async (err, academicnews) => {
-    //     await ScholarshipNews.find({}, async (err, scholarshipnews) => {
-          const response = await axios.get("https://type.fit/api/quotes");
-          const random = Math.floor(Math.random() * response.data.length);
-          const data = response.data[random];
-     
-          await User.find({},async(err,users)=>{
-           await Subject.find({},(err,subjects)=>{
+    const response = await axios.get("https://type.fit/api/quotes");
+    const random = Math.floor(Math.random() * response.data.length);
+    const data = response.data[random];
 
-          res.render("index", {users,subjects,data});
-           })
-          })
-    //     }).sort({ created: "desc" });
-    //   }).sort({ created: "desc" });
-    // }).sort({ created: "desc" });
+    await User.find({}, async (err, users) => {
+      await Subject.find({}, (err, subjects) => {
+        res.render("index", { users, subjects, data });
+      });
+    });
   } catch (err) {
     return new AppError(err.message, err.status);
   }
 };
 
 exports.getDashboard = async (req, res) => {
-  await User.find({}, (err, users) => {
-    User.findOne({name:req.user.name}).populate("subject_list").exec((err,user)=>{
-
-    res.render("dashboard", { users,user });
-    })
+  await User.find({}, async(err, users) => {
+   await User.findOne({ name: req.user.name })
+      .populate("subject_list")
+      .exec((err, user) => {
+        res.render("dashboard", { users, user });
+      });
   });
 };
 
@@ -44,6 +37,6 @@ exports.logout = (req, res) => {
   res.redirect("/");
 };
 
-exports.back  = (req,res) => {
-  res.redirect('back')
-}
+exports.back = (req, res) => {
+  res.redirect("back");
+};
