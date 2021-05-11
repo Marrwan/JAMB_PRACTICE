a046174e99349e4ae9baa63128e168ddca387db0
Marrwan <Abdulbasitdamilola6@gmail.com>
2021-05-11 10:37:02 +0100

    error

--- app.js
+++ app.js
@@ -70,9 +70,9 @@ app.all("*", (req, res, next) => {
 });
 
 // catch 404 and forward to error handler
-// app.use(function (req, res, next) {
-//   next(createError(404));
-// });
+app.use(function (req, res, next) {
+  next(createError(404));
+});
 
 // error handler
 app.use(function (err, req, res, next) {
